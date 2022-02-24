b0d8d279bf542b2c5ffd7e6ed69d10b1870fcf31
lhing17 <zxj12345>
2022-02-24 17:13:42 +0800

    feat: 引入netty。

--- src/lan_clip/core.clj
+++ src/lan_clip/core.clj
@@ -2,27 +2,21 @@
   (:import (java.awt Toolkit)
            (java.awt.datatransfer DataFlavor ClipboardOwner)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (def clip (.getSystemClipboard (Toolkit/getDefaultToolkit)))
 
 (defn- set-owner [clpbd owner]
-  (println owner)
   (.setContents clpbd (.getContents clpbd nil) owner))
 
 
-(defrecord Owner []
+(defrecord Owner
+  []
+
   ClipboardOwner
   (lostOwnership [this clipboard _]
     (Thread/sleep 1000)
     (when (.isDataFlavorAvailable clipboard DataFlavor/stringFlavor)
       (println (.getData clipboard DataFlavor/stringFlavor))
-      (set-owner clipboard this))
-   ))
-
+      (set-owner clipboard this))))
 
 
 (defn -main [& _]
